78fd8839165f97c0a27fde68bacd6e5aae1abf7a
Tiago Emerick <tiagoeborsanyi@gmail.com>
2022-12-14 20:46:11 -0300

    work on cardPage

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_banking/data_json/card_json.dart';
 import 'package:mobile_banking/theme/color.dart';
 
 class CardPage extends StatefulWidget {
@@ -17,6 +18,7 @@ class _CardPageState extends State<CardPage> {
         preferredSize: const Size.fromHeight(60),
         child: getAppBar(),
       ),
+      body: getBody(),
     );
   }
 
@@ -46,6 +48,77 @@ class _CardPageState extends State<CardPage> {
             Icons.more_vert,
             color: black,
             size: 25,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    final PageController controller = PageController();
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        SizedBox(
+          width: double.infinity,
+          height: 240,
+          child: PageView(
+            controller: controller,
+            children: List.generate(cardLists.length, (index) {
+              return getCards(
+                cardLists[index]['amount'],
+                cardLists[index]['currency'],
+                cardLists[index]['card_number'],
+                cardLists[index]['valid_date'],
+                cardLists[index]['bg_color'],
+              );
+            }),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget getCards(amount, currency, cardNumber, validDate, bgColor) {
+    var size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              currency,
+              style: const TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: black,
+              ),
+            ),
+            const SizedBox(width: 5),
+            Text(
+              amount,
+              style: const TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: black,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 15),
+        Container(
+          width: size.width * .85,
+          height: 170,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [],
           ),
         ),
       ],
